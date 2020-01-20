088d740b2a3fa202e42794cfa2e0722e043ccbae
Steve Birtles <sbirtles@farnborough.ac.uk>
2020-01-20 20:22:50 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const ws = require('ws');
-const uuidv4 = require('uuid/v4');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,40 +10,36 @@ const port = 8081;
 
 app.use('/client', express.static('client'));
 
+let serverStartTime;
 server.listen(port, function(){
+
   console.log(`Server started on port ${port}`);
+
+  serverStartTime = Date.now();
+
+  setInterval(() => {
+    let broadcast = `{"serverTime" : ${Date.now() - serverStartTime}}`;
+    console.log(broadcast);
+    for (let client of wsServer.clients) {
+        client.send(broadcast);
+    }
+  }, 50);
+
 });
 
 
+let clientCount = 0;
+
 wsServer.on('connection', client => {
 
-    client.id = uuidv4();
-    client.send(`<strong>Welcome!</strong> You are client ${client.id}.`);
-
-    let greeting = `<strong>Client ${client.id} joined!</strong>`;
-    for (let c of wsServer.clients) {
-        if (c != client) c.send(greeting);
-    }
-    console.log(greeting);
+    clientCount++;
+    client.id = clientCount;
+    client.send(`{"id":${client.id}}`);
 
     client.on('message', message => {
-
-        let broadcast = `<strong>Client ${client.id}:</strong> ${message}`;
-        for (let c of wsServer.clients) {
-            if (c != client) c.send(broadcast);
-        }
-        console.log(broadcast);
-
     });
 
     client.on('close', () => {
-
-      let farewell = `<strong>Client ${client.id} disconnected!</strong>`;
-      for (let c of wsServer.clients) {
-          if (c != client) c.send(farewell);
-      }
-      console.log(farewell);
-
     });
 
 });
